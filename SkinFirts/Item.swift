7ae9d3dd5954d86145cbac35e66295a476e8fee8
emmanuel <isiwuemmma@gmail.com>
2025-05-23 18:04:42 +0100

    Initial Commit

---
+++ SkinFirts/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SkinFirts
+//
+//  Created by Emmanuel Isiwu on 23/05/2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
